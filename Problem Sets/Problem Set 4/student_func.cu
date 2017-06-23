14c40ca4e4e678b9768989fda7d809c47d7fbee2
Thien Bui <thienbui797@gmail.com>
2017-06-23 23:18:02 +1000

    lession 4

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -3,7 +3,7 @@
 
 #include "utils.h"
 #include <thrust/host_vector.h>
-
+#include <stdio.h>
 /* Red Eye Removal
    ===============
    
@@ -41,7 +41,67 @@
    at the end.
 
  */
+__global__ void counter(unsigned int* d_count0,
+                unsigned int* d_count1,
+                unsigned int* const d_inputVals,
+                const unsigned int mask)
+{
+  unsigned int idx = blockIdx.x*blockDim.x + threadIdx.x;
+  unsigned int val = (d_inputVals[idx] >> mask) & 1;
+  if(!val){ 
+    d_count0[idx] = 1;
+    d_count1[idx] = 0;
+  }
+  else {
+    d_count1[idx] = 1;
+    d_count0[idx] = 0;
+  }
+}
 
+__global__ void preSum(unsigned int* d_in, unsigned int* d_out)
+{
+  unsigned int blockStart = blockIdx.x*blockDim.x;
+  unsigned int idx = blockStart + threadIdx.x;
+  unsigned int temp = d_in[idx];
+  __syncthreads();
+  for(int s = 1; s < blockDim.x; s <<=1){
+      unsigned int value = 0;
+      if(idx >= blockStart + s){
+        value = d_in[idx - s];
+      }
+      __syncthreads();
+      d_in[idx] += value;
+      __syncthreads();
+  }
+  if(idx == blockStart + blockDim.x - 1)
+  {
+    d_out[blockIdx.x] = d_in[idx]; 
+    //printf("%d %d %d\n",idx,blockStart,blockIdx.x);
+  }
+  d_in[idx] -= temp;
+}
+
+__global__ void assignValue(unsigned int* const d_inputVals,
+               unsigned int* const d_inputPos,
+               unsigned int* const d_outputVals,
+               unsigned int* const d_outputPos,
+               unsigned int* const d_skip,
+               unsigned int* const d_grid0,
+               unsigned int* const d_grid1,
+               unsigned int* const d_count0,
+               unsigned int* const d_count1,
+               const unsigned int mask)
+{
+  unsigned int idx = blockIdx.x*blockDim.x + threadIdx.x;
+  unsigned int val = (d_inputVals[idx] >> mask) & 1;
+  unsigned int next_idx;
+  if(val)
+    next_idx = d_skip[0] + d_grid1[blockIdx.x] + d_count1[idx];
+  else
+    next_idx = d_grid0[blockIdx.x] + d_count0[idx];
+  d_outputVals[next_idx] = d_inputVals[idx];
+  d_outputPos[next_idx]  = d_inputPos[idx];
+}
 
 void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_inputPos,
@@ -49,6 +109,47 @@ void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_outputPos,
                const size_t numElems)
 { 
-  //TODO
-  //PUT YOUR SORT HERE
+  const unsigned int gridSize = 320;
+  const unsigned int blockSize = 689;
+
+  unsigned int *d_skip;
+  unsigned int *d_count0,*d_count1;
+  unsigned int *d_grid0, *d_grid1 ;
+  checkCudaErrors(cudaMalloc(&d_count0, sizeof(unsigned int)*numElems));
+  checkCudaErrors(cudaMalloc(&d_count1, sizeof(unsigned int)*numElems));
+  checkCudaErrors(cudaMalloc(&d_grid0, sizeof(unsigned int)*gridSize));
+  checkCudaErrors(cudaMalloc(&d_grid1, sizeof(unsigned int)*gridSize));
+  checkCudaErrors(cudaMalloc(&d_skip, sizeof(unsigned int)*2));
+  
+  checkCudaErrors(cudaMemcpy(d_outputPos , d_inputPos , sizeof(unsigned int)*numElems, cudaMemcpyDeviceToDevice));
+  checkCudaErrors(cudaMemcpy(d_outputVals, d_inputVals, sizeof(unsigned int)*numElems, cudaMemcpyDeviceToDevice));
+
+  for (unsigned int i = 0; i < 32; i ++) {
+    checkCudaErrors(cudaMemcpy(d_inputPos , d_outputPos , sizeof(unsigned int)*numElems, cudaMemcpyDeviceToDevice));
+    checkCudaErrors(cudaMemcpy(d_inputVals, d_outputVals, sizeof(unsigned int)*numElems, cudaMemcpyDeviceToDevice));
+
+    counter <<<gridSize, blockSize>>> (d_count0, d_count1, d_inputVals, i);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    preSum <<<gridSize, blockSize>>> (d_count0, d_grid0);
+    preSum <<<1, gridSize>>> (d_grid0, d_skip);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    preSum <<<gridSize, blockSize>>> (d_count1, d_grid1);
+    preSum <<<1, gridSize>>> (d_grid1, d_skip + 1);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    assignValue<<<gridSize, blockSize>>> (d_inputVals,
+                                          d_inputPos,
+                                          d_outputVals,
+                                          d_outputPos,
+                                          d_skip,
+                                          d_grid0,
+                                          d_grid1,
+                                          d_count0,
+                                          d_count1,i); 
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+ 
+  }
+
 }
